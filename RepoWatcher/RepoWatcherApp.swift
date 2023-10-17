778506ccba7750a0e77180ecf35a26742cd87025
Charles Roberts <cjroberts2013@gmail.com>
2023-10-17 16:42:38 -0500

    Initial Commit

---
+++ RepoWatcher/RepoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoWatcherApp.swift
+//  RepoWatcher
+//
+//  Created by Charles Roberts on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct RepoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
